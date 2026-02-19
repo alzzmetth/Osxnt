a79fb9523a7eeb340a70c4c942bd5b1717f5db1d
Alzzmeth <aldriankeishaalvaro@gmail.com>
2026-02-19 17:10:40 +0700

    Update __init__.py

--- modules/webtrack/__init__.py
+++ modules/webtrack/__init__.py
@@ -1,8 +1,14 @@
+# modules/webtrack/__init__.py
+
 from .webtrack import track_web, extract_domain, get_ip_from_domain, get_dns_records, get_whois_info
-from .webcode import process_single_target, process_multi_targets, download_website
+from .webcode import process_single_target, process_multi_targets
 
 __all__ = [
-    'track_web', 'extract_domain', 'get_ip_from_domain',
-    'get_dns_records', 'get_whois_info',
-    'process_single_target', 'process_multi_targets', 'download_website'
+    'track_web',
+    'extract_domain',
+    'get_ip_from_domain',
+    'get_dns_records',
+    'get_whois_info',
+    'process_single_target',
+    'process_multi_targets'
 ]
