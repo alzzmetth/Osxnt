16e2c59d813b0fc2348fb127adf8ae455060f016
Alzzmeth <aldriankeishaalvaro@gmail.com>
2026-02-19 13:25:06 +0700

    Create about.py

---
+++ core/about.py
@@ -0,0 +1,7 @@
+from .banner import show_banner
+
+def about():
+    show_banner()
+    print("Version: 1.0")
+    print("Author: alzzdevmaret")
+    print("Description: OSINT Toolkit for IP, Web, DNS, and Subdomain enumeration")
