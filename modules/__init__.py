ed1538b98e02678e299f75fd2097bc0c09d3c376
Alzzmeth <aldriankeishaalvaro@gmail.com>
2026-02-19 17:03:55 +0700

    Create __init__.py

---
+++ modules/__init__.py
@@ -0,0 +1,7 @@
+from . import iptrack
+from . import dns
+from . import scanport
+from . import subdomain
+from . import webtrack   # sekarang webtrack adalah package
+
+__all__ = ['iptrack', 'dns', 'scanport', 'subdomain', 'webtrack']
