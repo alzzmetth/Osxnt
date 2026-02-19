994fa8480156cf6e1a4b367330287de93821f2c8
Alzzmeth <aldriankeishaalvaro@gmail.com>
2026-02-19 17:47:08 +0700

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,7 +1,30 @@
+# modules/__init__.py
+# OSXNT - Main Modules Package
+# Upgraded with Spam Modules
+
+# Existing modules
 from . import iptrack
 from . import dns
 from . import scanport
 from . import subdomain
-from . import webtrack   # sekarang webtrack adalah package
+from . import webtrack
 
-__all__ = ['iptrack', 'dns', 'scanport', 'subdomain', 'webtrack']
+# New spam modules
+from .spam import NGLSpammer, GmailSpammer
+
+__all__ = [
+    # Existing
+    'iptrack',
+    'dns', 
+    'scanport',
+    'subdomain',
+    'webtrack',
+    
+    # New spam
+    'NGLSpammer',
+    'GmailSpammer'
+]
+
+# Version info
+__version__ = '2.1.0'
+__spam_version__ = '1.0.0'
