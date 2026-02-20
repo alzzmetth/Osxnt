231b785c7e1936687ef09c30a2aea7b572072ed3
Alzzmeth <aldriankeishaalvaro@gmail.com>
2026-02-20 09:05:25 +0700

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,6 +1,6 @@
 # modules/__init__.py
 # OSXNT - Main Modules Package
-# Upgraded with Spam Modules
+# Version: 2.2.0 (with HTTP & SSL Analyzer)
 
 # Existing modules
 from . import iptrack
@@ -9,7 +9,11 @@ from . import scanport
 from . import subdomain
 from . import webtrack
 
-# New spam modules
+# New analyzer modules
+from . import http_analyzer
+from . import ssl_analyzer
+
+# Spam modules
 from .spam import NGLSpammer, GmailSpammer
 
 __all__ = [
@@ -20,11 +24,21 @@ __all__ = [
     'subdomain',
     'webtrack',
     
-    # New spam
+    # New analyzers
+    'http_analyzer',
+    'ssl_analyzer',
+    
+    # Spam
     'NGLSpammer',
     'GmailSpammer'
 ]
 
 # Version info
-__version__ = '2.1.0'
+__version__ = '2.2.0'
+__http_version__ = '1.0.0'
+__ssl_version__ = '1.0.0'
 __spam_version__ = '1.0.0'
+
+# Untuk memudahkan import langsung dari modules
+from .http_analyzer import HTTPAnalyzer
+from .ssl_analyzer import SSLAnalyzer
