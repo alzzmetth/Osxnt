218458fa0c1233b590f7c04c92d6e9477876c8a5
Alzzmeth <aldriankeishaalvaro@gmail.com>
2026-02-22 13:34:12 +0700

    Update __init__.py

--- modules/darkweb/__init__.py
+++ modules/darkweb/__init__.py
@@ -1,3 +1,21 @@
-from .deployer import DarkWebDeployer
+# modules/darkweb/__init__.py
 
-__all__ = ['DarkWebDeployer']
+from .deployer import DarkWebDeployer
+from .tor_manager import TorManager
+from .server import DarkWebServer
+from .config import Config
+from .auth import DarkWebAuth
+from .monitor import DarkWebMonitor
+from .ui_darkweb import DarkWebUI
+
+__all__ = [
+    'DarkWebDeployer',
+    'TorManager',
+    'DarkWebServer',
+    'Config',
+    'DarkWebAuth',
+    'DarkWebMonitor',
+    'DarkWebUI'
+]
+
+__version__ = '1.1.0'
